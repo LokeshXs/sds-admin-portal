eedb26a4272f2b1d016f98df5359d2dc3186fe6b
LOKESH SINGH <lokesh1129ece@gmail.com>
2024-03-21 03:42:29 +0530

    fixed the overflow issue in dashboard page

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -2,5 +2,7 @@ import { auth } from "@/auth";
 
 export default async function DashboardHome() {
   const session = await auth();
-  return <main className="">{JSON.stringify(session)}</main>;
+  return <main className="overflow-x-hidden">
+    <h1 className="text-8xl max-md:text-6xl max-sm:text-3xl">Coming Soon</h1>
+  </main>;
 }
